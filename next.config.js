3594ab8752543a40922d3e1ea0a5971b7a8560c4
Aphiwad Chhoeun <72891986+aphiwadchh@users.noreply.github.com>
2022-11-08 12:09:22 -0800

    init project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config, options) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'nextremote',
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {
+            './HelloWorld': './pages/hello-world',
+          },
+          shared: {},
+        })
+      );
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
